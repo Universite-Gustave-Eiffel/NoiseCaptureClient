95b5e4980b757b41dc08f76e06770033fd71be9f
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2024-02-14 14:44:43 +0100

    Use brush to render a spectrum vertical gradient in the spectrogram chart

--- shared/src/commonMain/kotlin/org/noise_planet/noisecapture/shared/child/MeasurementScreen.kt
+++ shared/src/commonMain/kotlin/org/noise_planet/noisecapture/shared/child/MeasurementScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import com.bumble.appyx.components.backstack.BackStack
 import com.bumble.appyx.navigation.modality.BuildContext
@@ -28,6 +29,7 @@ import org.noise_planet.noisecapture.shared.signal.WindowAnalysis
 import org.noise_planet.noisecapture.shared.signal.get44100HZ
 import org.noise_planet.noisecapture.shared.signal.get48000HZ
 import kotlin.math.floor
+import kotlin.math.log10
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.pow
@@ -36,70 +38,98 @@ import kotlin.math.round
 
 const val FFT_SIZE = 2048
 const val FFT_HOP = 1024
-
-fun String.hexToArgb(): Int {
-    require(length == 7 && startsWith("#")) { "Invalid hex color format." }
-    val rgb = substring(1, 3).toInt(16) shl 16 or (substring(3, 5).toInt(16) shl 8) or substring(5, 7).toInt(16)
-    return rgb
+const val MAX_SPECTRUM = 320 // max spectrum displayed in the spectrogram
+fun parseColor(colorString: String): Int {
+    var color = colorString.substring(1).toLong(16)
+    if (colorString.length == 7) {
+        // Set the alpha value
+        color = color or 0x00000000ff000000L
+    } else if (colorString.length != 9) {
+        throw IllegalArgumentException("Unknown color")
+    }
+    return color.toInt()
 }
 
 fun String.toComposeColor(): Color {
-    return Color(hexToArgb())
+    return Color(parseColor(this))
 }
+
+enum class SCALE_MODE {
+    SCALE_LINEAR,
+    SCALE_LOG
+}
+
+val colorRamp = arrayOf(
+    "#303030".toComposeColor(),
+    "#2D3C2D".toComposeColor(),
+    "#2A482A".toComposeColor(),
+    "#275427".toComposeColor(),
+    "#246024".toComposeColor(),
+    "#216C21".toComposeColor(),
+    "#3F8E19".toComposeColor(),
+    "#61A514".toComposeColor(),
+    "#82BB0F".toComposeColor(),
+    "#A4D20A".toComposeColor(),
+    "#C5E805".toComposeColor(),
+    "#E7FF00".toComposeColor(),
+    "#EBD400".toComposeColor(),
+    "#EFAA00".toComposeColor(),
+    "#F37F00".toComposeColor(),
+    "#F75500".toComposeColor(),
+    "#FB2A00".toComposeColor(),
+)
+
+data class SpectrogramModel(val values: List<Brush>)
 
 
 class MeasurementScreen(buildContext: BuildContext, val backStack: BackStack<ScreenData>,
                         private val audioSource: AudioSource) : Node(buildContext) {
     private val spectrumChannel: SpectrumChannel = SpectrumChannel()
 
-    var rangedB = 40.0
-    var mindB = 35.0
+    var rangedB = 0.0
+    var mindB = Double.MAX_VALUE
 
-    val colorRamp = arrayOf(
-        "#303030".toComposeColor(),
-        "#2D3C2D".toComposeColor(),
-        "#2A482A".toComposeColor(),
-        "#275427".toComposeColor(),
-        "#246024".toComposeColor(),
-        "#216C21".toComposeColor(),
-        "#3F8E19".toComposeColor(),
-        "#61A514".toComposeColor(),
-        "#82BB0F".toComposeColor(),
-        "#A4D20A".toComposeColor(),
-        "#C5E805".toComposeColor(),
-        "#E7FF00".toComposeColor(),
-        "#EBD400".toComposeColor(),
-        "#EFAA00".toComposeColor(),
-        "#F37F00".toComposeColor(),
-        "#F75500".toComposeColor(),
-        "#FB2A00".toComposeColor(),
-    )
+
+    fun pushSpectrum(prepend : List<Brush>, spectrum : FloatArray,
+                     scaleMode : SCALE_MODE) : List<Brush> {
+        if (spectrum.isEmpty()) {
+            return prepend
+        }
+        // Update dynamic range dB->Color index
+        mindB = min(mindB, spectrum.filter { fl -> fl.isFinite() }.minOrNull()?.toDouble() ?: mindB)
+        rangedB = max(mindB+rangedB,
+            spectrum.filter { fl -> fl.isFinite() }.maxOrNull()?.toDouble()
+                ?: (mindB + rangedB)
+        ) - mindB
+        // map spectrum value with brush ratio and rendering color
+        val spectrumColor = spectrum.mapIndexed { index, magnitude ->
+            val colorIndex = max( 0, min( colorRamp.size - 1,
+                floor(((magnitude - mindB) / rangedB) * colorRamp.size).toInt() ))
+            val verticalRatio = when(scaleMode) {
+                SCALE_MODE.SCALE_LOG -> log10(spectrum.size/(index+1).toFloat())
+                else -> spectrum.size/index.toFloat()
+            }
+            verticalRatio to colorRamp[colorIndex]
+        }.toTypedArray()
+        return prepend.subList(max(0,prepend.size-MAX_SPECTRUM), prepend.size)
+            .plus(Brush.verticalGradient(colorStops = spectrumColor))
+    }
 
     @Composable
-    fun SpectrogramView(spectrumData: FloatArray) {
+    fun SpectrogramView(spectrumData: SpectrogramModel) {
         Canvas(modifier = Modifier.fillMaxSize()) {
-            //drawRect(Color.Black)
-            val stepSize = size.height / spectrumData.size
-            mindB = min(mindB, spectrumData.filter { fl -> fl.isFinite() }.minOrNull()?.toDouble() ?: mindB)
-            rangedB = max(mindB+rangedB,
-                spectrumData.filter { fl -> fl.isFinite() }.maxOrNull()?.toDouble()
-                    ?: (mindB + rangedB)
-            ) - mindB
-            drawRect(color = Color.Black)
-//            for ((index, magnitude) in spectrumData.withIndex()) {
-//                val yPos = ((index / spectrumData.size.toDouble()) * size.height).toFloat()
-//                val colorIndex = max(0, min(colorRamp.size - 1, floor(((magnitude - mindB)
-//                        / rangedB) * colorRamp.size).toInt()))
-//                drawLine(color = colorRamp[colorIndex] , start = Offset(size.width / 2, yPos),
-//                    end = Offset(size.width / 2, yPos+stepSize), strokeWidth = stepSize)
-//            }
+            val xStep = size.width/spectrumData.values.size
+            spectrumData.values.forEachIndexed() { index, brush ->
+                drawRect(brush = brush, size= Size(xStep, size.height),
+                    topLeft = Offset(index * xStep, 0F))
+            }
         }
     }
 
     @Composable
     override fun View(modifier: Modifier) {
         var noiseLevel by remember { mutableStateOf(0.0) }
-        var spectrumState by remember {  mutableStateOf(floatArrayOf()) }
+        var spectrumState by remember { mutableStateOf(SpectrogramModel(listOf())) }
 
         lifecycleScope.launch {
             audioSource.setup()
@@ -119,7 +149,8 @@ class MeasurementScreen(buildContext: BuildContext, val backStack: BackStack<Scr
                 windowAnalysis.pushSamples(
                     Clock.System.now().toEpochMilliseconds(), samplesWithGain)
                     .forEach {
-                        spectrumState = it.spectrum
+                        spectrumState = SpectrogramModel(pushSpectrum(spectrumState.values,
+                            it.spectrum, SCALE_MODE.SCALE_LINEAR))
                 }
             }
         }.invokeOnCompletion {
