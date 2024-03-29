be0cda7a07759c894b781f2f447f30375a05fe89
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2024-03-29 15:24:08 +0100

    functions measurement service

--- shared/src/commonMain/kotlin/org/noise_planet/noisecapture/shared/MeasurementService.kt
+++ shared/src/commonMain/kotlin/org/noise_planet/noisecapture/shared/MeasurementService.kt
@@ -1,14 +1,32 @@
 package org.noise_planet.noisecapture.shared
 
 import kotlinx.atomicfu.atomic
+import kotlin.properties.Delegates
+
+typealias AcousticIndicatorsCallback = (acousticIndicatorsData: AcousticIndicatorsData) -> Unit
 
 class MeasurementService {
     private val canceled = atomic(false)
     private val storageActivated = atomic(false)
+    var acousticIndicatorsObservers = mutableListOf<AcousticIndicatorsCallback>()
+    var lastAcousticIndicator: AcousticIndicatorsData by Delegates.observable(
+        AcousticIndicatorsData(0, 0.0, doubleArrayOf())
+    ) { property, oldValue, newValue ->
+        acousticIndicatorsObservers.forEach { it(newValue) }
+    }
 
-
+    /**
+     * Start collecting measurements to be forwarded to observers
+     */
     fun startRecording() {
         canceled.value = false
+
+    }
+
+    /**
+     * Store measurement in database
+     */
+    fun startStorage() {
 
     }
 
