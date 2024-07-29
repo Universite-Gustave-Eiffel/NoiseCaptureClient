2f46e6005f7e2f1fc2b763f08995a883a920ea96
Marceau Tonelli <marceau.tonelli@cerema.fr>
2024-07-29 16:52:19 +0200

    Remove last usage of Koin PrintLogger

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,21 +1,18 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-import org.koin.core.logger.PrintLogger
 import org.noiseplanet.noisecapture.initKoin
 import org.noiseplanet.noisecapture.platformModule
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
 
-    // TODO: Add a logger implementation that uses JS console
-    val logger = PrintLogger()
-
     ComposeViewport(document.body!!) {
         initKoin(
-            additionalModules = listOf(platformModule)
-        ).logger(logger)
-
+            additionalModules = listOf(
+                platformModule
+            )
+        )
         App()
     }
 }
