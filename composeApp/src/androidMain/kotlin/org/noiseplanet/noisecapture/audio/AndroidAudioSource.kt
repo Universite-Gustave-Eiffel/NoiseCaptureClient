238bf1940dcae8590585536e197da9433d17464f
Marceau Tonelli <marceau.tonelli@cerema.fr>
2024-11-25 16:30:58 +0100

    Properly manage audio source state for Android

--- composeApp/src/androidMain/kotlin/org/noiseplanet/noisecapture/audio/AndroidAudioSource.kt
+++ composeApp/src/androidMain/kotlin/org/noiseplanet/noisecapture/audio/AndroidAudioSource.kt
@@ -36,25 +36,68 @@ internal class AndroidAudioSource(
     override val stateFlow: Flow<AudioSourceState> = stateChannel.receiveAsFlow()
 
     override fun setup() {
+        if (state == AudioSourceState.READY) {
+            logger.debug("Audio source is already initialized, skipping setup.")
+            return
+        }
+        
         // Create a recorder that will process raw incoming audio into audio samples
         // and broadcast it through the channel.
         audioRecorder = AudioRecorder(audioSamplesChannel, logger)
-    }
-
-    override fun release() {
-        pause()
-        audioRecorder = null
-        audioThread = null
+        state = AudioSourceState.READY
     }
 
     override fun start() {
-        // Start audio recording in a background thread and return the channel as a Flow
-        audioThread = Thread(audioRecorder)
-        audioThread?.start()
+        when (state) {
+            AudioSourceState.UNINITIALIZED -> {
+                logger.error("Audio source not initialized. Call setup() first.")
+                return
+            }
+
+            AudioSourceState.RUNNING -> {
+                logger.debug("Audio source already running.")
+                return
+            }
+
+            AudioSourceState.READY -> {
+                logger.debug("Audio source already paused.")
+                // Start audio recording in a background thread and return the channel as a Flow
+                audioThread = Thread(audioRecorder)
+                audioThread?.start()
+                state = AudioSourceState.RUNNING
+            }
+        }
     }
 
     override fun pause() {
-        audioRecorder?.stopRecording()
+        when (state) {
+            AudioSourceState.UNINITIALIZED -> {
+                logger.error("Audio source not initialized. Call setup() first.")
+                return
+            }
+
+            AudioSourceState.RUNNING -> {
+                audioRecorder?.stopRecording()
+                state = AudioSourceState.READY
+            }
+
+            AudioSourceState.READY -> {
+                logger.debug("Audio source already paused.")
+                return
+            }
+        }
+    }
+
+    override fun release() {
+        if (state == AudioSourceState.UNINITIALIZED) {
+            logger.debug("Audio source already uninitialized, skipping cleanup.")
+            return
+        }
+
+        pause()
+        audioRecorder = null
+        audioThread = null
+        state = AudioSourceState.UNINITIALIZED
     }
 
     override fun getMicrophoneLocation(): MicrophoneLocation {
