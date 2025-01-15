b226298a626e9bc4664f3c71aeec663cfd52eab1
Marceau Tonelli <marceau.tonelli@cerema.fr>
2025-01-15 16:51:05 +0100

    Get rid of menu items

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/home/HomeModule.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/home/HomeModule.kt
@@ -1,17 +1,9 @@
 package org.noiseplanet.noisecapture.ui.features.home
 
-import androidx.compose.ui.graphics.vector.ImageVector
-import org.jetbrains.compose.resources.StringResource
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
-import org.noiseplanet.noisecapture.ui.features.home.menuitem.MenuItemViewModel
-import org.noiseplanet.noisecapture.ui.navigation.Route
 
 val homeModule = module {
-
-    viewModel { (label: StringResource, imageVector: ImageVector, route: Route?) ->
-        MenuItemViewModel(label, imageVector, route)
-    }
 
     viewModel { (onClickSettingsButton: () -> Unit) ->
         HomeScreenViewModel(onClickSettingsButton)
