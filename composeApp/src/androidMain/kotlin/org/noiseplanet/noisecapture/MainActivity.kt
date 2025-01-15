95acdc9ed4028078b1b2f333b8f882e70e9f89aa
Marceau Tonelli <marceau.tonelli@cerema.fr>
2025-01-15 10:14:10 +0100

    App bar and status bar styling

--- composeApp/src/androidMain/kotlin/org/noiseplanet/noisecapture/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/noiseplanet/noisecapture/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import org.koin.dsl.module
 
 /**
@@ -15,6 +16,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         initKoin(
             additionalModules = listOf(
