03802a39b6315705f99a6a80844377909822fe92
Marceau Tonelli <marceau.tonelli@cerema.fr>
2025-02-26 14:10:52 +0100

    Added WasmJS UserAgent implementation

--- composeApp/src/wasmJsMain/kotlin/Platform.wasmjs.kt
+++ composeApp/src/wasmJsMain/kotlin/Platform.wasmjs.kt
@@ -1,8 +1,27 @@
+import org.noiseplanet.noisecapture.interop.navigator
+import org.noiseplanet.noisecapture.model.UserAgent
 import org.noiseplanet.noisecapture.permission.Permission
 
 class WasmJSPlatform : Platform {
 
-    override val name: String = "Web with Kotlin/Wasm"
+    override val userAgent: UserAgent
+        // Getting device information on web can be tricky due to the variety of
+        // browsers and devices. For now, this is the approach we take:
+        get() = UserAgent(
+            // On some browsers, will return the name of the *browser* manufacturer (e.g. Google Inc.)
+            deviceManufacturer = navigator?.vendor,
+            // Should return the platform running the browser (kind of device name, e.g. MacIntel)
+            deviceModelName = navigator?.platform,
+            // Returns either a fixed version number or a more detailed browser version string.
+            // In most browsers this is included as part of user agent.
+            deviceModelCode = navigator?.appCodeName,
+            // Most device information will be contained here but the format can change from browser
+            // to browser so better include the raw string than trying to parse it.
+            osVersion = navigator?.userAgent,
+            // Using navigator.oscpu only works on Firefox an crashes on other browsers so we
+            // can't rely on that. To filter based on OS, one should rely on [osVersion] instead.
+            osName = "WasmJS",
+        )
 
     override val requiredPermissions: List<Permission>
         // We can't control laptop settings on the web so we don't
