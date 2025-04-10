327c8747d1ffe98a35515193c344449a2371fe74
Marceau Tonelli <marceau.tonelli@cerema.fr>
2025-04-10 13:59:06 +0200

    Small code cleanup

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/measurement/plot/spectrogram/SpectrogramPlotViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/measurement/plot/spectrogram/SpectrogramPlotViewModel.kt
@@ -36,7 +36,7 @@ class SpectrogramPlotViewModel : ViewModel(), KoinComponent {
 
     // - Properties
 
-    private val measurementsService: LiveAudioService by inject()
+    private val liveAudioService: LiveAudioService by inject()
     private val logger: Logger by injectLogger()
 
     private var canvasSize: IntSize = IntSize.Zero
@@ -44,7 +44,7 @@ class SpectrogramPlotViewModel : ViewModel(), KoinComponent {
 
     val scaleMode = SpectrogramScaleMode.SCALE_LOG
 
-    val sampleRateFlow: Flow<Double> = measurementsService
+    val sampleRateFlow: Flow<Double> = liveAudioService
         .getSpectrumDataFlow()
         .map { it.sampleRate.toDouble() }
 
@@ -60,7 +60,7 @@ class SpectrogramPlotViewModel : ViewModel(), KoinComponent {
     init {
         viewModelScope.launch {
             // Listen to spectrum data updates and build spectrogram along the way
-            measurementsService.getSpectrumDataFlow()
+            liveAudioService.getSpectrumDataFlow()
                 .collect { spectrumData ->
                     currentStripData?.let { currentStripData ->
                         // Update current strip data
