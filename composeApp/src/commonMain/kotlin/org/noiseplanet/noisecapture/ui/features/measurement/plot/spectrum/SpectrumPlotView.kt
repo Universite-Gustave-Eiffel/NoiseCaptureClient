0ab6a6b6a75cada1fbfa2ee3727e95c7d92e103d
Marceau Tonelli <marceau.tonelli@cerema.fr>
2025-04-10 13:54:37 +0200

    Use new map object instead of double array in spectrum

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/measurement/plot/spectrum/SpectrumPlotView.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/measurement/plot/spectrum/SpectrumPlotView.kt
@@ -53,9 +53,9 @@ fun SpectrumPlotView(
         0
     )
 
-    val rawSpl: Map<Int, Double> by viewModel.rawSplFlow
+    val rawSplPerThirdOctave: Map<Int, Double> by viewModel.rawSplFlow
         .collectAsState(emptyMap())
-    val weightedSpl: Map<Int, Double> by viewModel.weightedSplFlow
+    val weightedSplPerThirdOctave: Map<Int, Double> by viewModel.weightedSplFlow
         .collectAsState(emptyMap())
     val axisSettings: SpectrumPlotViewModel.AxisSettings by viewModel.axisSettingsFlow
         .collectAsState(
@@ -76,16 +76,18 @@ fun SpectrumPlotView(
         val barMaxWidth: Float = size.width - maxYAxisWidth
         val maxXAxisHeight = preparedSpectrumOverlayBitmap.horizontalLegendSize.height
         val chartHeight = (size.height - maxXAxisHeight - axisBuilder.tickLength.toPx())
-        val barHeight = chartHeight / rawSpl.size - SPECTRUM_PLOT_SQUARE_OFFSET.toPx()
+        val barHeight = chartHeight / rawSplPerThirdOctave.size - SPECTRUM_PLOT_SQUARE_OFFSET.toPx()
 
 
         // TODO: Use new map property instead of double array
 
-        rawSpl.forEachIndexed { index, spl ->
+        rawSplPerThirdOctave.keys.forEachIndexed { index, frequency ->
+            val spl = rawSplPerThirdOctave[frequency] ?: return@forEachIndexed
+            val weightedSpl = weightedSplPerThirdOctave[frequency] ?: return@forEachIndexed
             val barYOffset =
-                (barHeight + SPECTRUM_PLOT_SQUARE_OFFSET.toPx()) * (rawSpl.size - 1 - index)
+                (barHeight + SPECTRUM_PLOT_SQUARE_OFFSET.toPx()) * (rawSplPerThirdOctave.size - 1 - index)
             val splRatio = (spl - DBA_MIN) / (DBA_MAX - DBA_MIN)
-            val splWeighted = max(spl, weightedSpl[index])
+            val splWeighted = max(spl, weightedSpl)
             val splWeightedRatio = min(
                 1.0,
                 max(
