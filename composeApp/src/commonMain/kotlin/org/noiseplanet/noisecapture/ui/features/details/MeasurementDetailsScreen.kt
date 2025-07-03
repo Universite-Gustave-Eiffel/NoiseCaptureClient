2ce6d7d21c792524531192a34d230362ece8cc05
Marceau Tonelli <marceau.tonelli@cerema.fr>
2025-07-03 17:24:16 +0200

    Fine tune nested scrolling in bottom sheet scaffold

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/details/MeasurementDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/details/MeasurementDetailsScreen.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.CircularProgressIndicator
@@ -23,17 +26,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import noisecapture.composeapp.generated.resources.Res
 import noisecapture.composeapp.generated.resources.measurement_details_loading_hint
 import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.koinInject
 import org.koin.compose.module.rememberKoinModules
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.noiseplanet.noisecapture.log.Logger
 
 
 @OptIn(KoinExperimentalAPI::class)
@@ -52,6 +58,10 @@ fun MeasurementDetailsScreen(
 
     // - Properties
 
+    val logger: Logger = koinInject()
+    val localDensity = LocalDensity.current
+    var containerHeight by remember { mutableStateOf(0.dp) }
+
     val viewState by viewModel.viewState.collectAsStateWithLifecycle()
     val isLoading: Boolean = viewState is MeasurementDetailsScreenViewState.Loading
 
@@ -61,10 +71,30 @@ fun MeasurementDetailsScreen(
             skipHiddenState = !isLoading
         ),
     )
-    var containerHeight by remember {
-        mutableStateOf(0.dp)
+
+    // Subscribe to sheet state updates to get the current bottom sheet offset
+    val currentBottomSheetOffset: Dp = try {
+        with(localDensity) { sheetState.bottomSheetState.requireOffset().toDp() }
+    } catch (e: IllegalStateException) {
+        logger.debug("Offset is not available yet: $e")
+        Dp.Infinity
     }
-    val localDensity = LocalDensity.current
+
+    // Animate sheet corner radius when expanding to the top to blend with top app bar
+    val sheetShape = RoundedCornerShape(
+        topStart = min(currentBottomSheetOffset, 28.dp),
+        topEnd = min(currentBottomSheetOffset, 28.dp),
+        bottomStart = 0.dp,
+        bottomEnd = 0.dp
+    )
+
+    // Collapse drag handle when fully expanded, and add a bit of padding to the top of the content
+    val dragHandleHeight = min(currentBottomSheetOffset, 48.dp)
+    val sheetContentTopPadding = 24.dp - dragHandleHeight / 2
+
+    // Only allow dismissing the sheet by swiping when content is fully scrolled at the top
+    val sheetContentScrollState = rememberScrollState()
+    val enableSheetSwipe = !(currentBottomSheetOffset == 0.dp && sheetContentScrollState.value > 0)
 
 
     // - Layout
@@ -74,10 +104,16 @@ fun MeasurementDetailsScreen(
 
     BottomSheetScaffold(
         scaffoldState = sheetState,
-        sheetShape = BottomSheetDefaults.ExpandedShape,
+        sheetShape = sheetShape,
+        sheetSwipeEnabled = enableSheetSwipe,
         sheetContainerColor = MaterialTheme.colorScheme.surfaceContainer,
-        containerColor = Color.White,
+        containerColor = MaterialTheme.colorScheme.surface,
         sheetPeekHeight = containerHeight * 0.33f,
+        sheetDragHandle = {
+            BottomSheetDefaults.DragHandle(
+                modifier = Modifier.height(dragHandleHeight)
+            )
+        },
         sheetContent = {
             when (viewState) {
                 is MeasurementDetailsScreenViewState.ContentReady -> {
@@ -85,6 +121,8 @@ fun MeasurementDetailsScreen(
                     MeasurementDetailsChartsView(
                         state.measurement.uuid,
                         modifier = Modifier.padding(horizontal = 16.dp)
+                            .verticalScroll(sheetContentScrollState)
+                            .padding(top = sheetContentTopPadding)
                     )
                 }
 
