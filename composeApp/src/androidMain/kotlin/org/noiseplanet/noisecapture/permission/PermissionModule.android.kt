ff0d656d006975842a3a38a8852f6a3084d58cd9
nicolas-f <github@nettrader.fr>
2025-08-01 14:54:37 +0200

    unused import

--- composeApp/src/androidMain/kotlin/org/noiseplanet/noisecapture/permission/PermissionModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/noiseplanet/noisecapture/permission/PermissionModule.android.kt
@@ -3,7 +3,6 @@ package org.noiseplanet.noisecapture.permission
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.location.LocationManager
-import android.os.Build
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
