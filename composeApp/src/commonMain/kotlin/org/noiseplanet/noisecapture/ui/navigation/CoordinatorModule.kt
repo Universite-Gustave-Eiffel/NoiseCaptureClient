841ec0420e6681d7778abea6102e91f17f968486
Marceau Tonelli <marceau.tonelli@cerema.fr>
2025-10-02 16:18:32 +0200

    Added a first version of MockMeasurementBuilder to generate debug measurement data

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/navigation/CoordinatorModule.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/navigation/CoordinatorModule.kt
@@ -6,6 +6,7 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import org.noiseplanet.noisecapture.ui.features.permission.PermissionPrompt
 import org.noiseplanet.noisecapture.ui.features.permission.RequestPermissionModalViewModel
+import org.noiseplanet.noisecapture.util.MockMeasurementBuilder
 
 
 val coordinatorModule = module {
@@ -16,5 +17,9 @@ val coordinatorModule = module {
 
     viewModel { (permissionPromptFlow: SharedFlow<PermissionPrompt?>) ->
         RequestPermissionModalViewModel(permissionPromptFlow)
+    }
+
+    single<MockMeasurementBuilder> {
+        MockMeasurementBuilder(measurementService = get())
     }
 }
