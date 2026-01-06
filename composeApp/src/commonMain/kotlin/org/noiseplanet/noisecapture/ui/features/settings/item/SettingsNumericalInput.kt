84d03368a851f242618274a8f257d88b6b009e5d
Marceau Tonelli <marceau.tonelli@cerema.fr>
2026-01-06 09:41:40 +0100

    Edition of number fields is now done in a new popup

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/settings/item/SettingsNumericalInput.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/features/settings/item/SettingsNumericalInput.kt
@@ -1,39 +1,40 @@
 package org.noiseplanet.noisecapture.ui.features.settings.item
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Velocity
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import noisecapture.composeapp.generated.resources.Res
+import noisecapture.composeapp.generated.resources.cancel
+import noisecapture.composeapp.generated.resources.save
+import org.jetbrains.compose.resources.stringResource
+import org.noiseplanet.noisecapture.ui.components.button.NCButton
+import org.noiseplanet.noisecapture.ui.components.button.NCButtonColors
+import org.noiseplanet.noisecapture.ui.components.button.NCButtonStyle
+import org.noiseplanet.noisecapture.ui.components.button.NCButtonViewModel
 
 /**
  * A custom text field for numerical settings values.
@@ -46,116 +47,114 @@ fun <T : Any> SettingsNumericalInput(
     viewModel: SettingsItemViewModel<T>,
     modifier: Modifier = Modifier,
 ) {
+    // - Properties
+
     var textFieldValueState by remember {
-        val initialValue = viewModel.getValue().toString()
-
-        mutableStateOf(
-            TextFieldValue(
-                text = initialValue,
-                selection = TextRange(initialValue.length)
-            )
-        )
+        mutableStateOf(viewModel.getValue().toString())
     }
-
-    val focusManager = LocalFocusManager.current
-    val interactionSource = remember { MutableInteractionSource() }
-
-    val colors = TextFieldDefaults.colors(
-        unfocusedContainerColor = Color.Transparent,
-        unfocusedIndicatorColor = Color.Transparent,
-    )
     val isEnabled by viewModel.isEnabled.collectAsState(true)
+
+    var showEditDialog: Boolean by remember { mutableStateOf(false) }
+    val confirmButtonViewModel = NCButtonViewModel(
+        title = Res.string.save,
+        colors = { NCButtonColors.Defaults.secondary() }
+    )
+    val cancelButtonViewModel = NCButtonViewModel(
+        title = Res.string.cancel,
+        style = NCButtonStyle.TEXT,
+        colors = { NCButtonColors.Defaults.text() }
+    )
+
+
+    // - Private functions
 
     @Suppress("UNCHECKED_CAST")
     fun getNumericalValue(): T? {
+        // TODO: Add value range validation
         return when (viewModel.settingKey.defaultValue) {
-            is Int -> textFieldValueState.text.toIntOrNull() as T?
-            is UInt -> textFieldValueState.text.toUIntOrNull() as T?
-            is Long -> textFieldValueState.text.toLongOrNull() as T?
-            is ULong -> textFieldValueState.text.toULongOrNull() as T?
-            is Double -> textFieldValueState.text.toDoubleOrNull() as T?
-            is Float -> textFieldValueState.text.toFloatOrNull() as T?
+            is Int -> textFieldValueState.toIntOrNull() as T?
+            is UInt -> textFieldValueState.toUIntOrNull() as T?
+            is Long -> textFieldValueState.toLongOrNull() as T?
+            is ULong -> textFieldValueState.toULongOrNull() as T?
+            is Double -> textFieldValueState.toDoubleOrNull() as T?
+            is Float -> textFieldValueState.toFloatOrNull() as T?
             else -> null
         }
     }
 
+    fun saveEdit() {
+        getNumericalValue()?.let { newValue ->
+            viewModel.setValue(newValue)
+            showEditDialog = false
+        }
+    }
+
+    fun cancelEdit() {
+        textFieldValueState = viewModel.getValue().toString()
+        showEditDialog = false
+    }
+
+
+    // - Layout
+
     Box(
         modifier = modifier.width(IntrinsicSize.Min)
+            .clickable {
+                showEditDialog = true
+            }
+            .padding(vertical = 16.dp)
     ) {
-        BasicTextField(
-            value = textFieldValueState,
-            onValueChange = { newValue ->
-                textFieldValueState = newValue
-                // If entered value is valid, save it
-                getNumericalValue()?.let {
-                    viewModel.setValue(it)
-                }
-            },
-            textStyle = MaterialTheme.typography.titleMedium.copy(
+        Text(
+            text = viewModel.getValue().toString(),
+            style = MaterialTheme.typography.titleMedium.copy(
                 textAlign = TextAlign.End,
                 color = MaterialTheme.colorScheme.onSurface
                     .copy(alpha = if (isEnabled) 1.0f else 0.5f)
             ),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Decimal,
-                imeAction = ImeAction.Done,
-            ),
-            keyboardActions = KeyboardActions(onDone = {
-                if (getNumericalValue() != null) {
-                    // Clear focus and dismiss keyboard if input is valid
-                    focusManager.clearFocus()
+            modifier = Modifier.widthIn(min = 32.dp, max = 64.dp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+
+    if (showEditDialog) {
+        AlertDialog(
+            title = {
+                Text(text = stringResource(viewModel.title))
+            },
+            text = {
+                Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                    Text(text = stringResource(viewModel.description))
+
+                    TextField(
+                        value = textFieldValueState,
+                        onValueChange = { newValue ->
+                            textFieldValueState = newValue
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Decimal,
+                            imeAction = ImeAction.Done,
+                        ),
+                        keyboardActions = KeyboardActions(onDone = { saveEdit() }),
+                        isError = getNumericalValue() == null,
+                        maxLines = 1,
+                    )
                 }
-            }),
-            singleLine = true,
-            enabled = isEnabled,
-            modifier = modifier
-                .widthIn(min = 32.dp, max = 64.dp)
-                .align(Alignment.CenterEnd)
-                .padding(vertical = 16.dp)
-                .padding(start = 4.dp)
-                .disabledHorizontalPointerInputScroll()
-                .onFocusChanged { focusState ->
-                    if (!focusState.isFocused) {
-                        textFieldValueState = textFieldValueState.copy(
-                            text = viewModel.getValue().toString()
-                        )
-                    }
-                }
-        ) {
-            TextFieldDefaults.DecorationBox(
-                value = textFieldValueState.text,
-                innerTextField = it,
-                singleLine = true,
-                enabled = true,
-                visualTransformation = VisualTransformation.None,
-                interactionSource = interactionSource,
-                isError = getNumericalValue() == null,
-                contentPadding = TextFieldDefaults.contentPaddingWithoutLabel(
-                    start = 0.dp,
-                    end = 0.dp,
-                    top = 0.dp,
-                    bottom = 0.dp,
-                ),
-                prefix = null,
-                colors = colors,
-            )
-        }
+            },
+            onDismissRequest = { cancelEdit() },
+            confirmButton = {
+                NCButton(
+                    viewModel = confirmButtonViewModel,
+                    onClick = { saveEdit() }
+                )
+            },
+            dismissButton = {
+                NCButton(
+                    viewModel = cancelButtonViewModel,
+                    onClick = { cancelEdit() }
+                )
+            },
+            modifier = Modifier.imePadding()
+        )
     }
 }
-
-
-/**
- * Fixes a weird scrolling behaviour when using BasicTextField with width based on IntrinsicSize.min:
- * https://stackoverflow.com/questions/73309395/strange-scroll-in-the-basictextfield-with-intrinsicsize-min
- */
-private val HorizontalScrollConsumer = object : NestedScrollConnection {
-    override fun onPreScroll(available: Offset, source: NestedScrollSource) = available.copy(y = 0f)
-    override suspend fun onPreFling(available: Velocity) = available.copy(y = 0f)
-}
-
-fun Modifier.disabledHorizontalPointerInputScroll(disabled: Boolean = true) =
-    if (disabled) {
-        this.nestedScroll(HorizontalScrollConsumer)
-    } else {
-        this
-    }
