9604806be0397a34402412ae3180b46f4f481f1e
Marceau Tonelli <marceau.tonelli@cerema.fr>
2026-01-15 11:19:55 +0100

    Add WasmJS support for file download

--- composeApp/src/wasmJsMain/kotlin/org/noiseplanet/noisecapture/services/storage/OPFSFileSystemService.kt
+++ composeApp/src/wasmJsMain/kotlin/org/noiseplanet/noisecapture/services/storage/OPFSFileSystemService.kt
@@ -1,8 +1,11 @@
 package org.noiseplanet.noisecapture.services.storage
 
+import kotlinx.browser.document
 import kotlinx.coroutines.await
 import org.noiseplanet.noisecapture.interop.storage.FileSystemWritableFileStream
 import org.noiseplanet.noisecapture.util.OPFSHelper
+import org.w3c.dom.HTMLAnchorElement
+import org.w3c.dom.url.URL
 import org.w3c.files.Blob
 import org.w3c.files.File
 
@@ -30,6 +33,26 @@ class OPFSFileSystemService : FileSystemService {
      * Using OPFS, root storage point is just an empty URL.
      */
     override fun getRootDirectory(): String = ""
+
+    override suspend fun downloadFile(fileUri: String) {
+        // Get file blob
+        val (fileHandle, _) = OPFSHelper.getFileHandle(fileUri) ?: return
+        val file: File = fileHandle.getFile().await()
+
+        // Create download URL
+        val url = URL.createObjectURL(file)
+
+        // Create anchor element and trigger the download
+        val anchor = document.createElement("a") as HTMLAnchorElement
+        anchor.href = url
+        anchor.download = file.name
+        document.body?.appendChild(anchor)
+        anchor.click()
+
+        // Clean up
+        document.body?.removeChild(anchor)
+        URL.revokeObjectURL(url)
+    }
 
     /**
      * Store a Blob of audio in persistent storage.
