c3ec029de9666d311c6eebd97a5cc1571066d73f
Marceau Tonelli <marceau.tonelli@cerema.fr>
2026-01-22 11:15:16 +0100

    Starting migration to compose 1.10

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -76,13 +76,13 @@ kotlin {
         }
 
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.materialIconsExtended)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.compose.runtime)
+//            implementation(compose.foundation)
+//            implementation(compose.material3)
+//            implementation(compose.materialIconsExtended)
+//            implementation(compose.ui)
+//            implementation(compose.components.resources)
+//            implementation(compose.components.uiToolingPreview)
 
             implementation(libs.androidx.navigation.compose)
             implementation(libs.androidx.viewmodel.compose)
