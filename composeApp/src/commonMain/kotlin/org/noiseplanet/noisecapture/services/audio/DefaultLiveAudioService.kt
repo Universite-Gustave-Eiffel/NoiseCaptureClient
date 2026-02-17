4fffc6ab48f1aecefef767141581f2b02c014482
Marceau Tonelli <marceau.tonelli@cerema.fr>
2026-02-17 12:09:58 +0100

    Update live audio service to only process acoustic indicators and spectrogram data on demand

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/services/audio/DefaultLiveAudioService.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/services/audio/DefaultLiveAudioService.kt
@@ -2,20 +2,21 @@ package org.noiseplanet.noisecapture.services.audio
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.noiseplanet.noisecapture.audio.AcousticIndicatorsProcessing
+import org.noiseplanet.noisecapture.audio.AudioSamples
 import org.noiseplanet.noisecapture.audio.AudioSource
 import org.noiseplanet.noisecapture.audio.signal.LevelDisplayWeightedDecay
 import org.noiseplanet.noisecapture.audio.signal.window.SpectrogramData
@@ -35,6 +36,7 @@ import kotlin.time.Duration
  * Default [LiveAudioService] implementation.
  * Can be overridden in platforms to add specific behaviour.
  */
+@OptIn(ExperimentalCoroutinesApi::class)
 class DefaultLiveAudioService : LiveAudioService, KoinComponent {
 
     // - Constants
@@ -58,16 +60,21 @@ class DefaultLiveAudioService : LiveAudioService, KoinComponent {
     private var indicatorsProcessing: AcousticIndicatorsProcessing? = null
     private var spectrogramDataProcessing: SpectrogramDataProcessing? = null
 
-    private var audioJob: Job? = null
-    private val leqRecordsFlow = MutableSharedFlow<LeqRecord>(
-        replay = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
-    private val spectrogramDataFlow = MutableSharedFlow<SpectrogramData>(
-        replay = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
+    private val leqRecordsFlow = audioSource.audioSamples
+        .flatMapConcat {
+            val processedSamples = processRawSamples(it)
+            flowOf(*processedSamples.toTypedArray())
+        }
+        .shareIn(scope = scope, started = SharingStarted.WhileSubscribed(), replay = 1)
+
+    private val spectrogramDataFlow = audioSource.audioSamples
+        .flatMapConcat {
+            val processedSamples = processSpectrogramData(it)
+            flowOf(*processedSamples.toTypedArray())
+        }
+        .shareIn(scope = scope, started = SharingStarted.WhileSubscribed(), replay = 1)
 
 
     // - LiveAudioService
@@ -80,40 +87,6 @@ class DefaultLiveAudioService : LiveAudioService, KoinComponent {
 
 
     override fun setupAudioSource() {
-        // Create a job that will process incoming audio samples in a background thread
-        audioJob = scope.launch {
-            audioSource.audioSamples
-                .collect { audioSamples ->
-                    // Process acoustic indicators
-                    if (indicatorsProcessing?.sampleRate != audioSamples.sampleRate) {
-                        logger.debug("Processing audio indicators with sample rate of ${audioSamples.sampleRate}")
-                        indicatorsProcessing = AcousticIndicatorsProcessing(audioSamples.sampleRate)
-                    }
-                    indicatorsProcessing?.processSamples(audioSamples)
-                        ?.forEach {
-                            leqRecordsFlow.tryEmit(it)
-                        }
-
-                    // Process spectrogram data
-                    // TODO: Consider moving this to SpectrogramPlotViewModel so that FFT doesn't
-                    //       always run in background when we don't need it.
-                    if (spectrogramDataProcessing?.sampleRate != audioSamples.sampleRate) {
-                        logger.debug("Processing spectrum data with sample rate of ${audioSamples.sampleRate}")
-                        val windowingMode = settingsService.get(SettingsKey.SettingWindowingMode)
-                        spectrogramDataProcessing = SpectrogramDataProcessing(
-                            sampleRate = audioSamples.sampleRate,
-                            windowSize = FFT_SIZE,
-                            windowHop = FFT_HOP,
-                            applyHannWindow = windowingMode == SpectrogramWindowingMode.HANN
-                        )
-                    }
-                    spectrogramDataProcessing?.pushSamples(audioSamples.epoch, audioSamples.samples)
-                        ?.forEach {
-                            spectrogramDataFlow.tryEmit(it)
-                        }
-                }
-        }
-
         // Setup audio source whenever microphone permission is granted.
         scope.launch {
             permissionService.getPermissionStateFlow(Permission.RECORD_AUDIO)
@@ -136,9 +109,6 @@ class DefaultLiveAudioService : LiveAudioService, KoinComponent {
     }
 
     override fun releaseAudioSource() {
-        // Cancel processing job
-        audioJob?.cancel()
-        // Release audio source
         scope.launch { audioSource.release() }
     }
 
@@ -157,11 +127,11 @@ class DefaultLiveAudioService : LiveAudioService, KoinComponent {
     }
 
     override fun getLeqRecordsFlow(): Flow<LeqRecord> {
-        return leqRecordsFlow.asSharedFlow()
+        return leqRecordsFlow
     }
 
     override fun getSpectrogramDataFlow(): Flow<SpectrogramData> {
-        return spectrogramDataFlow.asSharedFlow()
+        return spectrogramDataFlow
     }
 
     override fun getWeightedLeqFlow(
@@ -170,10 +140,9 @@ class DefaultLiveAudioService : LiveAudioService, KoinComponent {
     ): Flow<Double> {
         val levelDisplay = LevelDisplayWeightedDecay(splDecayRate, windowTime)
 
-        return getLeqRecordsFlow()
-            .map {
-                levelDisplay.getWeightedValue(it.laeq)
-            }
+        return getLeqRecordsFlow().map {
+            levelDisplay.getWeightedValue(it.laeq)
+        }
     }
 
     override fun getWeightedLeqPerFrequencyBandFlow(
@@ -182,18 +151,44 @@ class DefaultLiveAudioService : LiveAudioService, KoinComponent {
     ): Flow<Map<Int, Double>> {
         var levelDisplayBands: Map<Int, LevelDisplayWeightedDecay>? = null
 
-        return getLeqRecordsFlow()
-            .map { indicators ->
-                if (levelDisplayBands == null) {
-                    levelDisplayBands = indicators.leqsPerThirdOctave.mapValues {
-                        LevelDisplayWeightedDecay(splDecayRate, windowTime)
-                    }
-                }
-                indicators.leqsPerThirdOctave.mapValues { entry ->
-                    levelDisplayBands[entry.key]
-                        ?.getWeightedValue(entry.value)
-                        ?: 0.0
+        return getLeqRecordsFlow().map { indicators ->
+            if (levelDisplayBands == null) {
+                levelDisplayBands = indicators.leqsPerThirdOctave.mapValues {
+                    LevelDisplayWeightedDecay(splDecayRate, windowTime)
                 }
             }
+            indicators.leqsPerThirdOctave.mapValues { entry ->
+                levelDisplayBands[entry.key]
+                    ?.getWeightedValue(entry.value)
+                    ?: 0.0
+            }
+        }
+    }
+
+
+    // - Private functions
+
+    private suspend fun processRawSamples(audioSamples: AudioSamples): List<LeqRecord> {
+        if (indicatorsProcessing?.sampleRate != audioSamples.sampleRate) {
+            logger.debug("Processing audio indicators with sample rate of ${audioSamples.sampleRate}")
+            indicatorsProcessing = AcousticIndicatorsProcessing(audioSamples.sampleRate)
+        }
+
+        return indicatorsProcessing?.processSamples(audioSamples).orEmpty()
+    }
+
+    private fun processSpectrogramData(audioSamples: AudioSamples): List<SpectrogramData> {
+        if (spectrogramDataProcessing?.sampleRate != audioSamples.sampleRate) {
+            logger.debug("Processing spectrum data with sample rate of ${audioSamples.sampleRate}")
+            val windowingMode = settingsService.get(SettingsKey.SettingWindowingMode)
+            spectrogramDataProcessing = SpectrogramDataProcessing(
+                sampleRate = audioSamples.sampleRate,
+                windowSize = FFT_SIZE,
+                windowHop = FFT_HOP,
+                applyHannWindow = windowingMode == SpectrogramWindowingMode.HANN
+            )
+        }
+        return spectrogramDataProcessing?.pushSamples(audioSamples.epoch, audioSamples.samples)
+            .orEmpty().toList()
     }
 }
