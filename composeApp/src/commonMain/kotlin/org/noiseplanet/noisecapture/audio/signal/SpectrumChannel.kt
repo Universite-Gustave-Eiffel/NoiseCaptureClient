8da509428bc8993d1b4623b24dcf6fa5e3dca5f8
Marceau Tonelli <marceau.tonelli@cerema.fr>
2026-02-17 12:10:18 +0100

    Ensure spectrogram data processing is running in a background thread(s)

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/audio/signal/SpectrumChannel.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/audio/signal/SpectrumChannel.kt
@@ -1,7 +1,8 @@
 package org.noiseplanet.noisecapture.audio.signal
 
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.noiseplanet.noisecapture.audio.signal.filter.BiquadFilter
 import org.noiseplanet.noisecapture.audio.signal.filter.DigitalFilter
 import kotlin.math.pow
@@ -130,7 +131,7 @@ class SpectrumChannel {
         for (cascadeIndex in iirFilters.indices) {
             val cascadeFilters: HashMap<Int, BiquadFilter> = iirFilters[cascadeIndex]
             if (parallel) {
-                coroutineScope {
+                withContext(Dispatchers.Default) {
                     for ((key, value) in cascadeFilters.entries) {
                         launch {
                             leqs[key] = value.filterThenLeq(lastFilterSamples)
