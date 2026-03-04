dd65cb268dedf401b3a6c8a2f0be46cbd66e5177
Marceau Tonelli <marceau.tonelli@cerema.fr>
2026-03-04 13:20:11 +0100

    Fixed navigation bug when deleting measurement

--- composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/navigation/router/DetailsRouter.kt
+++ composeApp/src/commonMain/kotlin/org/noiseplanet/noisecapture/ui/navigation/router/DetailsRouter.kt
@@ -2,6 +2,9 @@ package org.noiseplanet.noisecapture.ui.navigation.router
 
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
+import androidx.navigation.toRoute
+import org.noiseplanet.noisecapture.ui.navigation.Route
+import org.noiseplanet.noisecapture.ui.navigation.RouteIds
 
 /**
  * Handles navigating to new screens after user takes actions on the details screen.
@@ -14,6 +17,11 @@ class DetailsRouter(
     // - Public functions
 
     fun onMeasurementDeleted() {
+        if (navController.currentBackStackEntry?.toRoute<Route>()?.id != RouteIds.DETAILS) {
+            // Measurement deleted callback might trigger twice in some cases, so before popping
+            // backstack, assert that we are still on the details screen.
+            return
+        }
         navController.popBackStack()
     }
 }
