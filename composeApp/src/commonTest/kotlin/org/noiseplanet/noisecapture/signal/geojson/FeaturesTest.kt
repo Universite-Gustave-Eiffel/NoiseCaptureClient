7e490997340373da3eb1a68c2af94804f32f364a
Marceau Tonelli <marceau.tonelli@cerema.fr>
2026-04-01 16:22:28 +0200

    Added GeoJson builder and serialisation utilities

---
+++ composeApp/src/commonTest/kotlin/org/noiseplanet/noisecapture/signal/geojson/FeaturesTest.kt
@@ -0,0 +1,226 @@
+package org.noiseplanet.noisecapture.signal.geojson
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonPrimitive
+import org.noiseplanet.noisecapture.util.Feature
+import org.noiseplanet.noisecapture.util.LineString
+import org.noiseplanet.noisecapture.util.MultiLineString
+import org.noiseplanet.noisecapture.util.MultiPoint
+import org.noiseplanet.noisecapture.util.Point
+import org.noiseplanet.noisecapture.util.Polygon
+import org.noiseplanet.noisecapture.util.lineOf
+import org.noiseplanet.noisecapture.util.positionOf
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Test serialization of GeoJson Features.
+ * https://datatracker.ietf.org/doc/html/rfc7946#appendix-A
+ */
+class FeaturesTest {
+
+    val json = Json {
+        explicitNulls = false
+        prettyPrint = true
+    }
+
+    @Test
+    fun testPoint() {
+        val expected = """
+            {
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        100.0,
+                        0.0
+                    ]
+                }
+            }
+        """.trimIndent()
+        val feature = Feature(geometry = Point(positionOf(100.0, 0.0)))
+        assertEquals(expected, json.encodeToString(feature))
+    }
+
+    @Test
+    fun testLineString() {
+        val expected = """
+            {
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [
+                        [
+                            100.0,
+                            0.0
+                        ],
+                        [
+                            0.0,
+                            100.0
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+        val feature = Feature(
+            geometry = LineString(lineOf(positionOf(100.0, 0.0), positionOf(0.0, 100.0)))
+        )
+        assertEquals(expected, json.encodeToString(feature))
+    }
+
+    @Test
+    fun testPolygon() {
+        val expected = """
+            {
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [
+                            [
+                                100.0,
+                                0.0
+                            ],
+                            [
+                                101.0,
+                                0.0
+                            ],
+                            [
+                                101.0,
+                                1.0
+                            ],
+                            [
+                                100.0,
+                                1.0
+                            ],
+                            [
+                                100.0,
+                                0.0
+                            ]
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+        val feature = Feature(
+            geometry = Polygon(
+                listOf(
+                    lineOf(
+                        positionOf(100.0, 0.0),
+                        positionOf(101.0, 0.0),
+                        positionOf(101.0, 1.0),
+                        positionOf(100.0, 1.0),
+                        positionOf(100.0, 0.0),
+                    )
+                )
+            )
+        )
+        assertEquals(expected, json.encodeToString(feature))
+    }
+
+    @Test
+    fun testMultipoint() {
+        val expected = """
+            {
+                "geometry": {
+                    "type": "MultiPoint",
+                    "coordinates": [
+                        [
+                            100.0,
+                            0.0
+                        ],
+                        [
+                            101.0,
+                            0.0
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+        val feature = Feature(
+            geometry = MultiPoint(
+                listOf(
+                    positionOf(100.0, 0.0),
+                    positionOf(101.0, 0.0),
+                )
+            )
+        )
+        assertEquals(expected, json.encodeToString(feature))
+    }
+
+    @Test
+    fun testMultiLineString() {
+        val expected = """
+            {
+                "geometry": {
+                    "type": "MultiLineString",
+                    "coordinates": [
+                        [
+                            [
+                                100.0,
+                                0.0
+                            ],
+                            [
+                                101.0,
+                                0.0
+                            ]
+                        ],
+                        [
+                            [
+                                101.0,
+                                1.0
+                            ],
+                            [
+                                100.0,
+                                1.0
+                            ]
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+        val feature = Feature(
+            geometry = MultiLineString(
+                listOf(
+                    lineOf(
+                        positionOf(100.0, 0.0),
+                        positionOf(101.0, 0.0),
+                    ),
+                    lineOf(
+                        positionOf(101.0, 1.0),
+                        positionOf(100.0, 1.0),
+                    )
+                )
+            )
+        )
+        assertEquals(expected, json.encodeToString(feature))
+    }
+
+    @Test
+    fun testProperties() {
+        val expected = """
+            {
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        100.0,
+                        0.0
+                    ]
+                },
+                "properties": {
+                    "doubleTest": 0.0,
+                    "intTest": 1,
+                    "stringTest": "string",
+                    "boolTest": true
+                }
+            }
+        """.trimIndent()
+        val feature = Feature(
+            geometry = Point(positionOf(100.0, 0.0)),
+            properties = mapOf(
+                "doubleTest" to JsonPrimitive(0.0),
+                "intTest" to JsonPrimitive(1),
+                "stringTest" to JsonPrimitive("string"),
+                "boolTest" to JsonPrimitive(true)
+            )
+        )
+        assertEquals(expected, json.encodeToString(feature))
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/org/noiseplanet/noisecapture/signal/geojson/PositionTest.kt
@@ -0,0 +1,80 @@
+package org.noiseplanet.noisecapture.signal.geojson
+
+import kotlinx.serialization.json.Json
+import org.noiseplanet.noisecapture.util.alt
+import org.noiseplanet.noisecapture.util.lat
+import org.noiseplanet.noisecapture.util.lon
+import org.noiseplanet.noisecapture.util.positionOf
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNull
+
+
+/**
+ * Tests GeoJson's Position object
+ * https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.1
+ */
+class PositionTest {
+
+    val json = Json {
+        explicitNulls = false
+        prettyPrint = true
+    }
+
+    @Test
+    fun testXY() {
+        val expected = """
+            [
+                100.0,
+                0.0
+            ]
+        """.trimIndent()
+
+        val position = positionOf(100.0, 0.0)
+        assertEquals(expected, json.encodeToString(position))
+    }
+
+    @Test
+    fun testXYZ() {
+        val expected = """
+            [
+                100.0,
+                0.0,
+                50.0
+            ]
+        """.trimIndent()
+
+        val position = positionOf(100.0, 0.0, 50.0)
+        assertEquals(expected, json.encodeToString(position))
+    }
+
+    @Test
+    fun testLatLon() {
+        val position = positionOf(100.0, 0.0)
+        assertEquals(100.0, position.lon)
+        assertEquals(0.0, position.lat)
+        assertNull(position.alt)
+    }
+
+    @Test
+    fun testLatLonAlt() {
+        val position = positionOf(100.0, 0.0, 50.0)
+        assertEquals(100.0, position.lon)
+        assertEquals(0.0, position.lat)
+        assertEquals(50.0, position.alt)
+    }
+
+    @Test
+    fun testInvalidCoordinates() {
+        assertFailsWith(IllegalArgumentException::class) {
+            positionOf()
+        }
+        assertFailsWith(IllegalArgumentException::class) {
+            positionOf(1.0)
+        }
+        assertFailsWith(IllegalArgumentException::class) {
+            positionOf(1.0, 2.0, 3.0, 4.0)
+        }
+    }
+}
